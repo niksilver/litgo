fcdb95fc93a2d0841d2706b994541de8ac69b559
Nik Silver <nik@niksilver.com>
2019-10-28 20:35:34 +0000

    Starting to add a bit more logic to line processing

--- main.go
+++ main.go
@@ -9,6 +9,14 @@ import (
 	"strings"
 )
 
+// Package level declarations
+type processing interface {
+	proc(string)
+}
+type state struct {
+	markdown strings.Builder
+}
+
 // Functions
 func main() {
 	input := []byte("# Hello world\n\nThis is my other literate document")
@@ -16,10 +24,21 @@ func main() {
 	fmt.Println(string(output))
 }
 
-func processContent(c []byte, proc func(line string)) {
+func processContent(c []byte, p processing) {
 	r := strings.NewReader(string(c))
 	sc := bufio.NewScanner(r)
 	for sc.Scan() {
-		proc(sc.Text())
+		p.proc(sc.Text())
 	}
+}
+
+func (s *state) proc(line string) {
+	// Ignore lines that start with X
+	if strings.HasPrefix(line, "X") {
+		//return
+	}
+
+	// Do process other lines
+	//markdown.WriteString(line)
+
 }
